8bfa738c33cee319a4fd8ecc4be66f6b4796cf48
tomoyaki <tk5819555@gmail.com>
2021-07-25 13:28:45 +0900

    Initial Commit

---
+++ iOS Getting Started/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Getting Started
+//
+//  Created by Tomoya Kitazawa on 2021/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
